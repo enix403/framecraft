5ae15b90c6be6cc144458b21c43d69a74ef99367
enix403 <fog.code000@gmail.com>
2025-01-05 18:05:57 +0500

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import "./styles/reset.css";
+import "./styles/load-fonts";
+import "./styles/global.css";
 
-export function App() {
-  const [count, setCount] = useState(0);
+import { BrowserRouter, Routes, Route } from "react-router";
 
+// import Home from "./pages/home/Home";
+// import Login from "./pages/auth/Login";
+// import Register from "./pages/auth/Register";
+// import Generate from "./pages/generate/Generate";
+// import EditDesign from "./pages/generate/EditDesign";
+// import ViewRender from "./pages/generate/ViewRender";
+// import MyDesigns from "./pages/generate/MyDesigns";
+
+const App = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/*
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/generate" element={<Generate />} />
+        <Route path="/edit" element={<EditDesign />} />
+        <Route path="/render" element={<ViewRender />} />
+        <Route path="/my-designs" element={<MyDesigns />} />
+        */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;
