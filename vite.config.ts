3c6a4379ef9edbc8993efea1b5f0585bdf12a2d6
enix403 <fog.code000@gmail.com>
2025-01-05 17:51:34 +0500

    Create blank react project

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  }
+});
