9e19e5ad885ad0290122db744ac48f657811306f
enix403 <fog.code000@gmail.com>
2025-02-25 14:51:10 +0500

    Changes

--- src/editor/EditorPage.tsx
+++ src/editor/EditorPage.tsx
@@ -1,15 +1,16 @@
 import clsx from 'clsx';
+
 import { repeatNode } from '~/utils/markup';
+
+import planJson from './plan.json';
+(window as any).planJson = planJson;
+
 
 export function EditorPage() {
   return (
     <div className={clsx(
       'w-full max-w-full h-full max-h-full',
-      'flex flex-col overflow-auto'
     )}>
-      {repeatNode(100, (i) => (
-        <div>Hello World {i}</div>
-      ))}
     </div>
   );
 }
