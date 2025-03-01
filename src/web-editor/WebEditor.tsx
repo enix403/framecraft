cd16bb96fea20cc46439742f47e44342fa7c6962
enix403 <fog.code000@gmail.com>
2025-03-02 00:48:33 +0500

    install shadcn

--- src/web-editor/WebEditor.tsx
+++ src/web-editor/WebEditor.tsx
@@ -1,9 +1,13 @@
-import { repeatNode } from "~/utils/markup";
+import { Button } from "@/components/ui/button";
+import { repeatNode } from "@/utils/markup";
 
 export function WebEditor() {
   return (
-    <p className="text-2xl text-purple-400 font-bold">
-      FrameCraft
-    </p>
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <Button>Click me</Button>
+    </div>
+    // <p className="text-2xl text-purple-400 font-bold">
+    //   FrameCraft
+    // </p>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), tailwindcss(), react()],
 
   server: {
     port: 3000,
