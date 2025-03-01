f9ac85401cb6807fedc25bd55297c59c257a93a6
enix403 <fog.code000@gmail.com>
2025-03-02 00:58:37 +0500

    Setup shadcn

--- src/web-editor/WebEditor.tsx
+++ src/web-editor/WebEditor.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { repeatNode } from "@/utils/markup";
 
 export function WebEditor() {
   return (
-    <p className="text-2xl text-purple-400 font-bold">
-      FrameCraft
-    </p>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+    // <p className="text-2xl text-purple-400 font-bold">
+    //   FrameCraft
+    // </p>
   );
 }
