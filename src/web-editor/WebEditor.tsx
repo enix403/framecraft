0574ab7c06559e5880c0edb84fff79dff23518e5
enix403 <fog.code000@gmail.com>
2025-03-02 01:06:36 +0500

    Reset

--- src/web-editor/WebEditor.tsx
+++ src/web-editor/WebEditor.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { repeatNode } from "@/utils/markup";
 
 export function WebEditor() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-    // <p className="text-2xl text-purple-400 font-bold">
-    //   FrameCraft
-    // </p>
+    <p className="text-2xl text-purple-400 font-bold">
+      FrameCraft
+    </p>
   );
 }
