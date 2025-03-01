eb36e86638ce2f452d7fba8aecf6546979db44db
enix403 <fog.code000@gmail.com>
2025-03-02 02:43:52 +0500

    start editor

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
   {
     variants: {
       variant: {
@@ -19,10 +19,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "size-9",
+        default: "px-4 py-2",
+        sm: "rounded-md px-3 py-2 text-xs",
+        lg: "rounded-md px-8 py-2.5",
+        icon: "px-2 py-2",
       },
     },
     defaultVariants: {
--- src/web-editor/WebEditor.tsx
+++ src/web-editor/WebEditor.tsx
@@ -1,26 +1,35 @@
 import { Button } from "@/components/ui/button";
-
-import { ChevronDownIcon, Sparkle } from "lucide-react";
+import { ChevronDownIcon, Settings, Sparkle } from "lucide-react";
 
 export function WebEditor() {
   return (
-    <div className='flex items-center border-b px-2 py-4'>
-      <Button variant='secondary' className='h-auto w-auto py-3'>
-        <Sparkle className='!h-5 !w-5' />
-        <ChevronDownIcon className='opacity-60' aria-hidden='true' />
-      </Button>
+    <nav className='flex border-b px-2 py-4'>
+      <div className='flex flex-1 items-center'>
+        <Button variant='secondary' className='h-auto w-auto py-3'>
+          <Sparkle className='!h-5 !w-5' />
+          <ChevronDownIcon className='opacity-60' aria-hidden='true' />
+        </Button>
 
-      <Button variant='ghost' className='ml-4 py-6'>
-        <p className='max-w-40 overflow-hidden text-ellipsis'>
-          <span className="text-gray-500">Projects / </span>
-          My Plan 1
-        </p>
-        <ChevronDownIcon
-          className='-me-1 opacity-60'
-          size={16}
-          aria-hidden='true'
-        />
-      </Button>
-    </div>
+        <Button variant='ghost' className='ml-4 py-4'>
+          <p className='max-w-40 overflow-hidden text-ellipsis'>
+            <span className='text-gray-500'>Projects / </span>
+            My Plan 1
+          </p>
+          <ChevronDownIcon
+            className='-me-1 opacity-60'
+            size={16}
+            aria-hidden='true'
+          />
+        </Button>
+      </div>
+      <div className='flex flex-1 items-center justify-end'>
+        <Button size='lg' className="mr-2">
+          Export
+        </Button>
+        <Button variant='outline' size='icon'>
+          <Settings />
+        </Button>
+      </div>
+    </nav>
   );
 }
