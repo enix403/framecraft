369e157f1f8a0cc00d85dd56bfdbac79db552e6a
enix403 <fog.code000@gmail.com>
2025-03-02 00:54:35 +0500

    Remove shadcn

--- src/web-editor/WebEditor.tsx
+++ src/web-editor/WebEditor.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { repeatNode } from "@/utils/markup";
 
 export function WebEditor() {
   return (
-    <div className='flex min-h-svh flex-col items-center justify-center'>
-      <Button>Click me</Button>
-    </div>
-    // <p className="text-2xl text-purple-400 font-bold">
-    //   FrameCraft
-    // </p>
+    <p className="text-2xl text-purple-400 font-bold">
+      FrameCraft
+    </p>
   );
 }
