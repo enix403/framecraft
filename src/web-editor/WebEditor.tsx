1408b36451cb7009c07fe39cb3d2f285b9ae2ff0
enix403 <fog.code000@gmail.com>
2025-03-02 13:49:01 +0500

    save

--- src/web-editor/WebEditor.tsx
+++ src/web-editor/WebEditor.tsx
@@ -23,6 +23,7 @@ import {
 } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useId } from "react";
+import { repeatNode } from "@/utils/markup";
 
 function TopNav() {
   return (
@@ -76,58 +77,92 @@ function TopNav() {
   );
 }
 
+function RoomList() {
+  return (
+    <div className='flex-1 flex flex-col p-4 pb-0'>
+      <h2 className='mb-2 font-semibold'>Rooms</h2>
+      <div className='relative'>
+        <div className='pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50'>
+          <Search size={16} aria-hidden='true' />
+        </div>
+        <Input
+          className='bg-muted ps-9 pe-11'
+          placeholder='Search...'
+          type='search'
+        />
+        <div className='pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-2 text-muted-foreground'>
+          <kbd className='inline-flex h-5 max-h-full items-center rounded border bg-primary-foreground px-1 font-[inherit] text-[0.625rem] font-medium text-muted-foreground/70'>
+            ⌘K
+          </kbd>
+        </div>
+      </div>
+
+      <div className='-mx-4 flex-1 mt-4'>
+        <div className="h-full max-h-full overflow-y-auto d">
+        {repeatNode(50, index => (
+          <button
+            key={index}
+            className='flex w-full items-center gap-x-2 px-4 py-3 hover:bg-accent'
+          >
+            <Eclipse className='text-[#f9e909]' strokeWidth={3} />
+            <span>Balcony {index + 1}</span>
+          </button>
+        ))}
+        </div>
+        {/* <button className='flex w-full items-center gap-x-2 px-4 py-3 hover:bg-accent'>
+          <Tv className='text-[#EE4D4D]' strokeWidth={3} />
+          <span>Living Room</span>
+        </button>
+        <button className='flex w-full items-center gap-x-2 bg-[#DDEDFE] px-4 py-3'>
+          <BedDouble className='text-[#a808c5]' strokeWidth={3} />
+          <span>Bedroom 1</span>
+        </button>
+        <button className='flex w-full items-center gap-x-2 px-4 py-3 hover:bg-accent'>
+          <BedDouble className='text-[#a808c5]' strokeWidth={3} />
+          <span>Bedroom 2</span>
+        </button>
+        <button className='flex w-full items-center gap-x-2 px-4 py-3 hover:bg-accent'>
+          <Droplet className='text-[#BEBEBE]' strokeWidth={3} />
+          <span>Bathroom</span>
+        </button>
+        <button className='flex w-full items-center gap-x-2 px-4 py-3 hover:bg-accent'>
+          <Grape className='text-[#6ce244]' strokeWidth={3} />
+          <span>Kitchen</span>
+        </button>
+        {repeatNode(10, index => (
+          <button
+            key={index}
+            className='flex w-full items-center gap-x-2 px-4 py-3 hover:bg-accent'
+          >
+            <Eclipse className='text-[#f9e909]' strokeWidth={3} />
+            <span>Balcony {index + 1}</span>
+          </button>
+        ))} */}
+      </div>
+    </div>
+  );
+}
+
+function GlobalStats() {
+  return <div className='shrink-0 border-t p-4'>daw</div>;
+}
+
+function LeftPane() {
+  return (
+    <>
+      <RoomList />
+      <GlobalStats />
+    </>
+  );
+}
+
 export function WebEditor() {
-  const id = useId();
   return (
     <div className='flex h-full max-h-full flex-col overflow-hidden'>
       <TopNav />
       <ResizablePanelGroup direction='horizontal'>
-        <ResizablePanel minSize={10} defaultSize={18} className='p-4'>
-          <h2 className='mb-2 font-semibold'>Rooms</h2>
-          <div className='relative'>
-            <div className='pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50'>
-              <Search size={16} aria-hidden='true' />
-            </div>
-            <Input
-              id={id}
-              className='bg-muted ps-9 pe-11'
-              placeholder='Search...'
-              type='search'
-            />
-            <div className='pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-2 text-muted-foreground'>
-              <kbd className='inline-flex h-5 max-h-full items-center rounded border bg-primary-foreground px-1 font-[inherit] text-[0.625rem] font-medium text-muted-foreground/70'>
-                ⌘K
-              </kbd>
-            </div>
-          </div>
-
-          <div className='-mx-4 mt-4'>
-            <button className='w-full flex items-center gap-x-2 px-4 py-3 hover:bg-accent'>
-              <Tv className='text-[#EE4D4D]' strokeWidth={3} />
-              <span>Living Room</span>
-            </button>
-            <button className='w-full flex items-center gap-x-2 bg-[#DDEDFE] px-4 py-3'>
-              <BedDouble className='text-[#a808c5]' strokeWidth={3} />
-              <span>Bedroom 1</span>
-            </button>
-            <button className='w-full flex items-center gap-x-2 px-4 py-3 hover:bg-accent'>
-              <BedDouble className='text-[#a808c5]' strokeWidth={3} />
-              <span>Bedroom 2</span>
-            </button>
-            <button className='w-full flex items-center gap-x-2 px-4 py-3 hover:bg-accent'>
-              <Droplet className='text-[#BEBEBE]' strokeWidth={3} />
-              <span>Bathroom</span>
-            </button>
-            <button className='w-full flex items-center gap-x-2 px-4 py-3 hover:bg-accent'>
-              <Grape className='text-[#6ce244]' strokeWidth={3} />
-              <span>Kitchen</span>
-            </button>
-            <button className='w-full flex items-center gap-x-2 px-4 py-3 hover:bg-accent'>
-              <Eclipse className='text-[#f9e909]' strokeWidth={3} />
-              <span>Balcony</span>
-            </button>
-          </div>
-
+        <ResizablePanel minSize={10} defaultSize={18} className='flex flex-col'>
+          <LeftPane />
         </ResizablePanel>
         <ResizableHandle />
         <ResizablePanel minSize={40}>Two</ResizablePanel>
