26967c89b195954d3222857d74268b1b730e4884
enix403 <fog.code000@gmail.com>
2025-03-03 16:15:14 +0500

    wall measures

--- src/scratch/Scratch.tsx
+++ src/scratch/Scratch.tsx
@@ -46,55 +46,6 @@ function calcLineRect(
   return { x, y, width, height };
 }
 
-function WallMeasure({ side, x, y, width, height, gap = 8, textGap = 8 }) {
-  let points: any[] = [];
-
-  {
-    const w = width - 1;
-    const h = height - 1;
-
-    if (side === "top") {
-      points = [x, y, x, y - gap, x + w, y - gap, x + w, y];
-    } else if (side === "bottom") {
-      points = [x, y + h, x, y + h + gap, x + w, y + h + gap, x + w, y + h];
-    } else if (side === "left") {
-      points = [x, y, x - gap, y, x - gap, y + h, x, y + h];
-    } else if (side === "right") {
-      points = [x + w, y, x + w + gap, y, x + w + gap, y + h, x + w, y + h];
-    }
-  }
-
-  let lineCenterX = Math.round((points[2] + points[4]) / 2);
-  let lineCenterY = Math.round((points[3] + points[5]) / 2);
-
-  const isHoriz = side === "top" || side === "bottom";
-
-  if (side == "top") lineCenterY -= textGap;
-  if (side == "bottom") lineCenterY += textGap;
-  if (side == "left") lineCenterX -= textGap;
-  if (side == "right") lineCenterX += textGap;
-
-  return (
-    <>
-      <Line points={points} stroke='#AAAAAA' />
-      <Text
-        text='1000ft'
-        fontSize={13}
-        fill='#848484'
-        x={lineCenterX}
-        y={lineCenterY}
-        width={1000}
-        height={1000}
-        offsetX={500}
-        offsetY={500}
-        align='center'
-        verticalAlign='middle'
-        rotationDeg={isHoriz ? 0 : 90}
-      />
-    </>
-  );
-}
-
 function RenderWalls({ plan }: { plan: any }) {
   return plan.walls.map(
     ({ id, row, col, length, direction, width: thickness }) => {
@@ -124,32 +75,82 @@ function RenderWalls({ plan }: { plan: any }) {
   );
 }
 
-function RenderWallMeasures({ plan }: { plan: any }) {
-  return plan.walls.map(
-    ({ row, col, length, direction, width: thickness }) => {
-      const { x, y, width, height } = calcLineRect(
-        row,
-        col,
-        length,
-        direction,
-        thickness
-      );
+function WallMeasure({ side, x, y, width, height, gap = 8, textGap = 8 }) {
+  let points: any[] = [];
 
-      return (
-        <>
-          {length > 25 && (
-            <WallMeasure
-              x={x}
-              y={y}
-              width={width}
-              height={height}
-              side={direction === "h" ? "top" : "right"}
-            />
-          )}
-        </>
-      );
+  {
+    const w = width - 1;
+    const h = height - 1;
+
+    if (side === "top") {
+      points = [x, y, x, y - gap, x + w, y - gap, x + w, y];
+    } else if (side === "bottom") {
+      points = [x, y + h, x, y + h + gap, x + w, y + h + gap, x + w, y + h];
+    } else if (side === "left") {
+      points = [x, y, x - gap, y, x - gap, y + h, x, y + h];
+    } else if (side === "right") {
+      points = [x + w, y, x + w + gap, y, x + w + gap, y + h, x + w, y + h];
     }
+  }
+
+  let lineCenterX = Math.round((points[2] + points[4]) / 2);
+  let lineCenterY = Math.round((points[3] + points[5]) / 2);
+
+  if (side == "top") lineCenterY -= textGap;
+  if (side == "bottom") lineCenterY += textGap;
+  if (side == "left") lineCenterX -= textGap;
+  if (side == "right") lineCenterX += textGap;
+
+  let rot = 0;
+  if (side === "right") {
+    rot = 90;
+  } else if (side === "left") {
+    rot = -90;
+  }
+
+  return (
+    <>
+      <Line points={points} stroke='#AAAAAA' />
+      <Text
+        text='1000ft'
+        fontSize={13}
+        fill='#848484'
+        x={lineCenterX}
+        y={lineCenterY}
+        width={1000}
+        height={1000}
+        offsetX={500}
+        offsetY={500}
+        align='center'
+        verticalAlign='middle'
+        rotationDeg={rot}
+      />
+    </>
   );
+}
+
+function RenderWallMeasures({ plan }: { plan: any }) {
+  return plan.walls.map(({ row, col, length, direction, width: thickness }) => {
+    if (length < 25) return null;
+
+    const { x, y, width, height } = calcLineRect(
+      row,
+      col,
+      length,
+      direction,
+      thickness
+    );
+
+    return (
+      <WallMeasure
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        side={direction === "h" ? "top" : "right"}
+      />
+    );
+  });
 }
 
 function RenderDoors({ plan }: { plan: any }) {
