50a70154b82c98159bdff51af509ec29a7e48da9
enix403 <fog.code000@gmail.com>
2025-03-05 15:27:26 +0500

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import "./styles/global.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
+import { Scratch } from "./pages/_scratch/Scratch";
 import { WebEditor } from "./pages/web-editor/WebEditor";
-import { Scratch } from "./pages/scratch/Scratch";
 
 export function App() {
   return (
     <TooltipProvider delayDuration={0}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<WebEditor />} />
           <Route path='/s' element={<Scratch />} />
+          <Route path='/' element={<WebEditor />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
---
+++ src/pages/generate/GenerateDesign.tsx
@@ -0,0 +1,3 @@
+export function GenerateDesign() {
+  return "GenerateDesign";
+}
