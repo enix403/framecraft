0f5c8451e56d2969d9221c509803c53069a3f0b4
enix403 <fog.code000@gmail.com>
2025-03-07 15:16:16 +0500

    refactor

--- src/pages/generate/Toolbar.tsx
+++ src/pages/generate/Toolbar.tsx
@@ -1,26 +1,31 @@
 import { RoomIdentityInput } from "@/components/RoomIdentityInput";
 import { TooltipWrapper } from "@/components/TooltipWrapper";
 import { Button } from "@/components/ui/button";
-import { Info, MessageSquareWarning, Redo, Undo } from "lucide-react";
+import { Info, Redo, Undo } from "lucide-react";
+import { useState } from "react";
 
 export function Toolbar() {
+  const [nodeSelected, setNodeSelected] = useState(false);
+
   return (
     <nav className='flex gap-x-2 border-b px-4 py-2'>
       <div className='flex flex-1 items-center'>
-        {/* <RoomIdentityInput
-          initialName={"Living Room 1"}
-          initialNodeType={0}
-          onUpdateName={name => {}}
-          onUpdateNodeType={type => {}}
-          className='flex-1 max-w-lg'
-        /> */}
-
-        <p className='flex items-center flex-1-x gap-x-2 text-muted-foreground/70'>
-          <Info className="shrink-0" />
-          <span className='font-medium tracking-tight'>
-            Select a node in the editor to inspect
-          </span>
-        </p>
+        {nodeSelected ? (
+          <RoomIdentityInput
+            initialName={"Living Room 1"}
+            initialNodeType={0}
+            onUpdateName={name => {}}
+            onUpdateNodeType={type => {}}
+            className='max-w-lg flex-1'
+          />
+        ) : (
+          <p className='flex flex-1-x items-center gap-x-2 text-muted-foreground/70'>
+            <Info className='shrink-0' />
+            <span className='font-medium tracking-tight'>
+              Select a node in the editor to inspect
+            </span>
+          </p>
+        )}
       </div>
       <div className='flex items-center justify-end'>
         <TooltipWrapper tip='Undo'>
