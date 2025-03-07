21320d56df2842d09a5dfe93516f3a33d3e797f4
enix403 <fog.code000@gmail.com>
2025-03-07 19:19:33 +0500

    presets

--- src/pages/generate/GraphPresets.tsx
+++ src/pages/generate/GraphPresets.tsx
@@ -1,8 +1,6 @@
+import { ResponsiveNetworkCanvas } from "@nivo/network";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-// @dwats-nocheck
-
-import { ResponsiveNetworkCanvas } from "@nivo/network";
 import { serverIdToNodeType } from "@/lib/nodes";
 import { appNodeStyle } from "@/lib/node-styles";
 import { repeatNode } from "@/lib/utils";
@@ -33,7 +31,7 @@ const data = {
 };
 
 const PresetPreview = () => (
-  <button className='h-[12rem] w-[12rem] rounded-2xl bg-indigo-50 tc hover:bg-indigo-100'>
+  <button className='h-[12rem] w-[12rem] shrink-0 rounded-2xl bg-indigo-50 tc hover:bg-indigo-100'>
     <ResponsiveNetworkCanvas
       data={data}
       isInteractive={false}
@@ -50,51 +48,30 @@ const PresetPreview = () => (
 export function GraphPresets() {
   return (
     <>
-      <div className='flex max-w-full gap-x-4 overflow-auto p-4'>
-        {repeatNode(16, index => (
+      <div className='flex items-center justify-between px-4 pt-2'>
+        <h2 className='text-xl font-bold tracking-tight'>Presets</h2>
+        <div className='flex items-center gap-x-2'>
+          <Button
+            variant='outline'
+            size='icon'
+            className='rounded-full border-blue-700 text-blue-700'
+          >
+            <ChevronLeft strokeWidth={3} />
+          </Button>
+          <Button
+            variant='outline'
+            size='icon'
+            className='rounded-full border-blue-700 text-blue-700'
+          >
+            <ChevronRight strokeWidth={3} />
+          </Button>
+        </div>
+      </div>
+      <div className='flex max-w-full gap-x-4 overflow-auto p-4 pt-2.5'>
+        {repeatNode(10, index => (
           <PresetPreview key={index} />
         ))}
-      </div>
-      <div className="flex items-center gap-x-2">
-        <Button
-          variant='outline'
-          size='icon'
-          className='rounded-full border-blue-700 text-blue-700'
-        >
-          <ChevronLeft strokeWidth={3} />
-        </Button>
-        <Button
-          variant='outline'
-          size='icon'
-          className='rounded-full border-blue-700 text-blue-700'
-        >
-          <ChevronRight strokeWidth={3} />
-        </Button>
       </div>
     </>
   );
 }
-
-/*
-
-
-return (
-  <div>
-    <h2 className='px-4 pt-2.5 text-xl font-bold tracking-tight'>Presets</h2>
-    <div className='flex'>
-      <button className='d'>
-        <ChevronLeft />
-      </button>
-      <div className='flex flex-1-x divide-x-2 py-2'>
-        {repeatNode(16, index => (
-          <PresetPreview key={index} />
-        ))}
-      </div>
-      <button className='d'>
-        <ChevronRight />
-      </button>
-    </div>
-  </div>
-);
-
-*/
