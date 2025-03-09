54f0f8a177e5aa1711a3d25bd61b5bc7d8c5b530
enix403 <fog.code000@gmail.com>
2025-03-09 11:49:06 +0500

    Add an info/alert on nodes drag sources

--- src/pages/generate/panes/NodeDragSource.tsx
+++ src/pages/generate/panes/NodeDragSource.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { appNodeTypes, NodeType } from "@/lib/nodes";
 import { NodeSlab } from "@/components/NodeSlab";
+import { Info } from "lucide-react";
 
 /* ========================== */
 
@@ -24,6 +25,13 @@ export function NodeDragSource() {
   return (
     <div className='flex h-full max-h-full w-[16rem] flex-col overflow-y-auto p-4'>
       <h2 className='mb-2 font-semibold'>Available Rooms</h2>
+
+      <div
+        className='mb-4 rounded-lg border border-blue-300 bg-blue-50 p-4 text-sm text-blue-800'
+        role='alert'
+      >
+        Drag nodes from here into the editor!
+      </div>
 
       <div className='-mx-4 -mb-4 flex-1-y space-y-2 px-4 pb-4'>
         {appNodeTypes.map(nodeType => {
