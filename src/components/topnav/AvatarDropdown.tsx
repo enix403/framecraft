ab4a53726c91761b001526d2c9a68b3f22f84148
enix403 <fog.code000@gmail.com>
2025-03-24 00:27:10 +0500

    changes

--- src/components/topnav/AvatarDropdown.tsx
+++ src/components/topnav/AvatarDropdown.tsx
@@ -8,10 +8,44 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
 import { useClearAuthState } from "@/stores/auth-store";
 
 import { LogOutIcon, PinIcon, UserPenIcon } from "lucide-react";
 import { Link, useNavigate } from "react-router";
+import { Spinner } from "../ui/spinner";
+
+function UserInfo() {
+  const { user, isError } = useCurrentUser();
+
+  if (user && !isError) {
+    return (
+      <>
+        <img
+          src='/profile_img_01.png'
+          alt='Avatar'
+          width={32}
+          height={32}
+          className='aspect-square shrink-0 rounded-full'
+        />
+        <div className='flex min-w-0 flex-col'>
+          <span className='truncate text-sm font-medium text-foreground'>
+            Keith Kennedy
+          </span>
+          <span className='truncate text-xs font-normal text-muted-foreground'>
+            k.kennedy@originui.com
+          </span>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className='flex flex-1 justify-center'>
+      <Spinner />
+    </div>
+  );
+}
 
 export function AvatarDropdown() {
   return (
@@ -19,11 +53,7 @@ export function AvatarDropdown() {
       <DropdownMenuTrigger asChild>
         <div className='relative cursor-pointer'>
           <Avatar className='size-10 border-2 border-[#FFF331]'>
-            <AvatarImage
-              // src='https://originui.com/avatar-80-03.jpg'
-              src='/profile_img_01.png'
-              alt='Kelly King'
-            />
+            <AvatarImage src='/profile_img_01.png' alt='Kelly King' />
             <AvatarFallback className='text-black'>U</AvatarFallback>
           </Avatar>
           {/* <span className='absolute -end-0.5 -bottom-0.5 size-3 rounded-full border-2 border-background bg-emerald-500'>
@@ -33,21 +63,7 @@ export function AvatarDropdown() {
       </DropdownMenuTrigger>
       <DropdownMenuContent className='max-w-64'>
         <DropdownMenuLabel className='flex items-start gap-3'>
-          <img
-            src='/profile_img_01.png'
-            alt='Avatar'
-            width={32}
-            height={32}
-            className='aspect-square shrink-0 rounded-full'
-          />
-          <div className='flex min-w-0 flex-col'>
-            <span className='truncate text-sm font-medium text-foreground'>
-              Keith Kennedy
-            </span>
-            <span className='truncate text-xs font-normal text-muted-foreground'>
-              k.kennedy@originui.com
-            </span>
-          </div>
+          <UserInfo />
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
