c12f9ab072ecc52fe2fdd56909145515980c7674
enix403 <fog.code000@gmail.com>
2025-03-27 19:11:10 +0500

    Tables

--- src/pages/_scratch/Scratch.tsx
+++ src/pages/_scratch/Scratch.tsx
@@ -1,3 +1,17 @@
+import { Ex1_BasicTable } from "./Ex1_BasicTable";
+import { Ex2_TableWithImages } from "./Ex2_TableWithImages";
+import { Ex3_DenseTable } from "./Ex3_DenseTable";
+import { Ex4_StickyHeader } from "./Ex4_StickyHeader";
+import { Ex5_BasicDataTable } from "./Ex5_BasicDataTable";
+
 export function Scratch() {
-  return null;
+  return (
+    <div className='p-8 space-y-7 max-h-full overflow-auto'>
+      <Ex1_BasicTable />
+      <Ex2_TableWithImages />
+      <Ex3_DenseTable />
+      <Ex4_StickyHeader />
+      <Ex5_BasicDataTable />
+    </div>
+  );
 }
