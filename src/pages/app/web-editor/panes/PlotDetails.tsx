1baffd8f9e1543ee9a61e3fc15f5f822640c57c3
enix403 <fog.code000@gmail.com>
2025-04-05 23:36:12 +0500

    plot details

--- src/pages/app/web-editor/panes/PlotDetails.tsx
+++ src/pages/app/web-editor/panes/PlotDetails.tsx
@@ -1,19 +1,33 @@
 import { RectPreview } from "@/components/RectPreview";
 import { Stat } from "@/components/Stat";
+import { usePlan } from "../plan-state";
 
 export function PlotDetails() {
+  const plan = usePlan();
+
+  let { plotWidth, plotLength, plotMeasureUnit } = plan;
+
+  const area = plotWidth * plotLength;
+
   return (
     <div className='shrink-0 border-t p-4 pb-16'>
       <h2 className='mb-4 font-semibold'>Plot Details</h2>
 
       <RectPreview
         className='mb-4'
-        rectangles={[{ left: 0, top: 0, width: 100, height: 110 }]}
+        rectangles={[
+          {
+            left: 0,
+            top: 0,
+            width: plotWidth,
+            height: plotLength
+          }
+        ]}
       />
 
-      <Stat label='Length' value='110 ft.' />
-      <Stat label='Width' value='100 ft.' />
-      <Stat label='Area' value='11000 ft. sq' />
+      <Stat label='Length' value={`${plotLength} ${plotMeasureUnit}`} />
+      <Stat label='Width' value={`${plotWidth} ${plotMeasureUnit}`} />
+      <Stat label='Area' value={`${area} ${plotMeasureUnit}. sq`} />
     </div>
   );
 }
